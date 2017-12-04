9bd0b14a2232dc7d85d589b5fac130b7e181f927
Fauzee26 <32213481+Fauzee26@users.noreply.github.com>
2017-12-04 13:54:42 +0700

    ok

---
+++ LetsMakeCake/LetsMakeCake/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  LetsMakeCake
+//
+//  Created by Muhammad Hilmy Fauzi on 26/11/17.
+//  Copyright © 2017 Hilmy Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelNama: UILabel!
+    @IBOutlet weak var labelBahan: UILabel!
+    @IBOutlet weak var labelCara: UILabel!
+
+    var passNama:String?
+    var passBahan:String?
+    var passCara:String?
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        labelNama.text = passNama
+        labelBahan.text = passBahan
+        labelCara.text = passCara
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
